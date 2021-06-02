239d23a6bbf704fa84b42089b0e107f8c0c83e54
GuilhermeJWT <guiromanno@gmail.com>
2021-06-02 12:42:42 -0300

    Pesquisando por Id

--- src/main/kotlin/br/com/systemsgs/forumalura/controller/TopicoController.kt
+++ src/main/kotlin/br/com/systemsgs/forumalura/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package br.com.systemsgs.forumalura.controller
 import br.com.systemsgs.forumalura.model.ModelTopico
 import br.com.systemsgs.forumalura.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -14,6 +15,11 @@ class TopicoController(private val topicoService: TopicoService) {
     @GetMapping("/listar")
     fun listar(): List<ModelTopico>{
         return topicoService.listar()
+    }
+
+    @GetMapping("/buscar/{id}")
+    fun buscarPorId(@PathVariable id: Long): ModelTopico{
+        return topicoService.buscarPorId(id)
     }
 
 }
--- src/main/kotlin/br/com/systemsgs/forumalura/service/TopicoService.kt
+++ src/main/kotlin/br/com/systemsgs/forumalura/service/TopicoService.kt
@@ -7,15 +7,23 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
+class TopicoService(private var topicos: List<ModelTopico>) {
 
-    fun listar(): List<ModelTopico>{
+    init {
         val topico = ModelTopico(id = 1, titulo = "Curso Kotlin", mensagem = "Curso sobre Kotlin e Api",
             curso = ModelCurso(id = 1, nome = "Kotlin", categoria = "Programação Mobile"),
             autor = ModelUsuario(id = 1, nome = "Guilherme Santos", email = "gui@zup.com"),
         )
 
-        return Arrays.asList(topico)
+        topicos =  Arrays.asList(topico)
+    }
+
+    fun listar(): List<ModelTopico>{
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): ModelTopico {
+        return topicos.stream().filter({t -> t.id == id}).findFirst().get()
     }
 
 }
