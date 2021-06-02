aacad17997c2165d17b191dab8a636a17ecea2e5
GuilhermeJWT <guiromanno@gmail.com>
2021-06-02 10:52:12 -0300

    Setup Projeto Kotlin & Spring Rest

---
+++ src/test/kotlin/br/com/systemsgs/forumalura/ForumAluraApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.systemsgs.forumalura
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumAluraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
