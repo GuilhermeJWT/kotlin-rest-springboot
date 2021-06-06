b9e09d2ad3e111b951dd5bfbb9b9016700b1e14e
GuilhermeJWT <guiromanno@gmail.com>
2021-06-06 15:12:57 -0300

    Criando os Relacionamentos entre as tabelas

--- src/main/kotlin/br/com/systemsgs/forumalura/model/ModelRespostas.kt
+++ src/main/kotlin/br/com/systemsgs/forumalura/model/ModelRespostas.kt
@@ -1,14 +1,25 @@
 package br.com.systemsgs.forumalura.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class ModelRespostas (
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
+
     val mensagem: String,
+
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+
     val solucao: Boolean,
+
+    @ManyToOne
     val autor: ModelUsuario,
+
+    @ManyToOne
     val topico: ModelTopico
 
 
--- src/main/kotlin/br/com/systemsgs/forumalura/model/ModelTopico.kt
+++ src/main/kotlin/br/com/systemsgs/forumalura/model/ModelTopico.kt
@@ -2,16 +2,31 @@ package br.com.systemsgs.forumalura.model
 
 import br.com.systemsgs.forumalura.enum.StatusTopico
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class ModelTopico (
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+
     val  titulo: String,
+
     val mensagem: String,
+
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+
+    @OneToMany(mappedBy = "topico")
     val respostas: List<ModelRespostas> = ArrayList(),
+
+    @ManyToOne
     val curso: ModelCurso,
+
+    @ManyToOne
     val autor: ModelUsuario,
+
+    @Enumerated(EnumType.STRING)
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO
 
 )
